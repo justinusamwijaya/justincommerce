dd87a3e6110f89ce535794d1cf6b2ee1f9881a79
Justinus Amadia Wijaya <justin.am.wijaya@gmail.com>
2018-07-05 08:02:09 +0700

    adding mlab into it

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 const routes = require('./routes');
 const cors = require('cors');
 
-mongoose.connect('mongodb://localhost:27017/ecommerce');
+mongoose.connect('mongodb://user1:12345a@ds129051.mlab.com:29051/ecommercaaa');
 
 app.use(cors())
 
